af79c6d825168473d5e21d307ee3a0fc7fe0f47d
JerryHan3 <13671233113@163.com>
2020-09-15 22:16:31 +0800

    First Commit
    Version: 0.0.1

---
+++ Assets/FileControllor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Common;
+
+public class FileControllor : MonoBehaviour 
+{
+    public string filepath;
+
+    public void OpenProject()
+    {
+        OpenFileDlg pth = new OpenFileDlg();
+        pth.structSize = System.Runtime.InteropServices.Marshal.SizeOf(pth);
+        pth.filter = "文本文档 (*.txt)";
+        pth.file = new string(new char[256]);
+        pth.maxFile = pth.file.Length;
+        pth.fileTitle = new string(new char[64]);
+        pth.maxFileTitle = pth.fileTitle.Length;
+        pth.initialDir = Application.dataPath;  // default path  
+        pth.title = "打开名单";
+        pth.defExt = "txt";
+        pth.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;
+        if (OpenFileDialog.GetOpenFileName(pth))
+        {
+            filepath = pth.file;//选择的文件路径;  
+            Debug.Log(filepath);
+        }
+    }
+
+    public void SaveProject()
+    {
+        SaveFileDlg pth = new SaveFileDlg();
+        pth.structSize = System.Runtime.InteropServices.Marshal.SizeOf(pth);
+        pth.filter = "txt (*.txt)";
+        pth.file = new string(new char[256]);
+        pth.maxFile = pth.file.Length;
+        pth.fileTitle = new string(new char[64]);
+        pth.maxFileTitle = pth.fileTitle.Length;
+        pth.initialDir = Application.dataPath;  // default path  
+        pth.title = "保存项目";
+        pth.defExt = "txt";
+        pth.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;
+        if (SaveFileDialog.GetSaveFileName(pth))
+        {
+            string filepath = pth.file;//选择的文件路径;  
+            Debug.Log(filepath);
+        }
+    }
+}
---
+++ Assets/StartUpCore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Text;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUpCore : MonoBehaviour
+{
+    public FileControllor Controller;
+    public Text NameShower, PathShower;
+    public string[] name;
+    private string namefilepath;
+
+    public void ReadName()
+    {
+        namefilepath = Controller.filepath;
+        name = File.ReadAllLines(namefilepath);
+        PathShower.text = namefilepath;
+    }
+
+    public void GenerateName()
+    {
+        int length = name.Length;
+        int id = Random.Range(0, length);
+        NameShower.text = name[id];
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
